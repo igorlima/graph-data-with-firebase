96362d62a856ff8a5dfb433fbf66d29747c0c78f
Igor Lima <lima.igorribeiro@gmail.com>
2015-08-25 16:05:43 -0300

    Clean up edges

--- dbaas.js
+++ dbaas.js
@@ -8,6 +8,34 @@ define(['jquery', 'backbone'], function($, Backbone) {
   vertexRef = ref.child('vertex');
   edgeRef   = ref.child('edge');
 
+  function removeEdgeIfVertexNotExist(edgeId, vertexId) {
+    vertexRef
+      .orderByKey()
+      .equalTo( vertexId )
+      .limitToFirst(1)
+      .once('value', function(data) {
+        !data.val() && edgeRef.child( edgeId ).remove();
+      } );
+  }
+
+  function removeEdgeIfNecessary(edgeId, edge) {
+    removeEdgeIfVertexNotExist(edgeId, edge.sourceId);
+    removeEdgeIfVertexNotExist(edgeId, edge.targetId);
+  }
+
+  function removeAllEdgesConnectedTo(vertexId) {
+    [ "sourceId", "targetId" ].forEach( function( variable ) {
+      edgeRef
+        .orderByChild( variable )
+        .equalTo( vertexId )
+        .once('value', function( data ) {
+          data.val() && Object.keys( data.val() ).forEach( function( edgeId ){
+            edgeRef.child( edgeId ).remove();
+          } )
+        });
+    } );
+  }
+
   Channel.on('disconnect', function() {
     console.warn('disconnected');
   });
@@ -40,44 +68,44 @@ define(['jquery', 'backbone'], function($, Backbone) {
       Channel.trigger( 'node-removed', {
         id: data.key()
       } );
+
+      removeAllEdgesConnectedTo( data.key() );
     });
 
     edgeRef.on('child_added', function(data) {
       var link = data.val(),
           isPlainObject = $.isPlainObject( link );
 
-      if ( !isPlainObject || !link.source || !link.target || !link.source.id || !link.target.id ) {
-        vertexRef.child( data.key() ).remove();
+      if ( !isPlainObject || !link.sourceId || !link.targetId ) {
+        edgeRef.child( data.key() ).remove();
         return;
       }
 
       Channel.trigger( 'link-added', {
         id: data.key(),
         source: {
-          id: data.val().source.id,
+          id: link.sourceId
         },
         target: {
-          id: data.val().target.id
+          id: link.targetId
         }
       } );
+
+      removeEdgeIfNecessary( data.key(), link );
     });
 
     edgeRef.on('child_removed', function(data) {
       var link = data.val(),
           isPlainObject = $.isPlainObject( link );
 
-      if ( !isPlainObject || !link.source || !link.target || !link.source.id || !link.target.id ) {
+      if ( !isPlainObject || !link.sourceId || !link.targetId ) {
         return;
       }
 
       Channel.trigger( 'link-removed', {
         id: data.key(),
-        source: {
-          id: data.val().source.id,
-        },
-        target: {
-          id: data.val().target.id
-        }
+        sourceId: data.val().sourceId,
+        targetId: data.val().targetId
       } );
     });
 
@@ -105,7 +133,7 @@ define(['jquery', 'backbone'], function($, Backbone) {
   } );
 
   Channel.on('remove-node', function(node) {
-    if (node && node.id) {
+    if ( node && node.id ) {
       vertexRef.child(node.id).remove();
     }
   });
@@ -114,12 +142,8 @@ define(['jquery', 'backbone'], function($, Backbone) {
     if (!link || !link.source || !link.target || !link.source.id || !link.target.id) return;
 
     var edge = edgeRef.push({
-      source: {
-        id: link.source.id
-      },
-      target: {
-        id: link.target.id
-      }
+      sourceId: link.source.id,
+      targetId: link.target.id
     }, function() {
       link.id = edge.key();
       Channel.trigger( 'link-added', link );
@@ -128,7 +152,7 @@ define(['jquery', 'backbone'], function($, Backbone) {
   });
 
   Channel.on('remove-link', function(link) {
-    if (link && link.id) {
+    if ( link && link.id ) {
       edgeRef.child(link.id).remove(function() {
         Channel.trigger( 'link-removed', link );
       });
@@ -137,4 +161,3 @@ define(['jquery', 'backbone'], function($, Backbone) {
 
   return Channel;
 });
-
